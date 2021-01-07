a085f3827d48ad84ffd9574e9b1705bb8f8f22bb
Steward Shih <steward.shih@quantatw.com>
2021-01-07 11:29:44 +0800

    Add uninstall script.

---
+++ uninstall_awesome_vimrc.sh
@@ -0,0 +1,8 @@
+if [ -e ~/.vim_runtime ]; then
+	echo "remove awesom vimrc"
+	rm -rf ~/.vim_runtime/autoload ~/.vim_runtime/plugged
+	rm ~/.vim*
+fi
+
+
+echo "Uninstalled the Ultimate Vim configuration....."
